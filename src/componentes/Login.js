ec06a489626c4b5f1a67ee469715c6a84d896766
margaritasing <libiamfreitesm@gmail.com>
2022-08-11 12:54:54 -0300

    modifico estilos en el login

--- src/componentes/Login.js
+++ src/componentes/Login.js
@@ -89,7 +89,7 @@ const Login = () => {
             </form>  
             <button
             onClick={handleGoogleSignin}
-            className="bg-slate-50 hover:bg-slate-200 text-black  shadow rounded border-2 border-gray-300 py-2 px-4 w-full"
+            className="text-white"
           >
             Google login
           </button>
